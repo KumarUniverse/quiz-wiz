9df27d27229f265d78cd85bf9eb849fe87be6c3b
Akash Kumar <AK887199@wcupa.edu>
2019-02-16 17:35:44 -0500

    Update quizlet-scraper.py

--- quizlet-scraper.py
+++ quizlet-scraper.py
@@ -1,3 +1,4 @@
+import uuid
 import urllib
 import urllib.request
 from bs4 import BeautifulSoup
@@ -5,16 +6,26 @@ import os
 
 page = 2
 # while page <= 2:
-optionsUrl = 'https://quizlet.com/subject/art-history/page/' + str(page) + '/?queryMeta=art+history'
-optionsPage = urllib.request.urlopen(optionsUrl)
+options_url = 'https://quizlet.com/subject/art-history/page/' + str(page) + '/?queryMeta=art+history'
+options_page = urllib.request.urlopen(options_url)
+questions_and_answers = {}
+soup = BeautifulSoup(options_page, "html.parser")
+link_list = soup.find_all('a', attrs={"class": "UILink"})
 
-soup = BeautifulSoup(optionsPage, "html.parser")
-linkList = soup.find_all('a', attrs={"class": "UILink"})
-for link in linkList:
+for link in link_list:
     quizlet = link.get('href')
     if quizlet.startswith('https'): # check if the link is a proper URL
         url = quizlet
+        page = urllib.request.urlopen(url)
+        baby_soup = BeautifulSoup(page, "html.parser")
 
+        # ques and answers: new changes
+        while: 
+            answer = soup.find('span', attrs={'class': "TermText notranslate lang-en"})
+            question = soup.find('span', attrs={'class': "TermText notranslate lang-en"})
+            questions_and_answers[question] = answer # add key-value pair
+            if soup.find('span', attrs={'class': "TermText notranslate lang-en"}) == NULL:
+                break
     #soup.findAll('p')
 
     # page += 1;
